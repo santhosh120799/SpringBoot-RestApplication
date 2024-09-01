b00b7e95580a638409f177529fb30dd59498bafc
Praveen <pravi15598@gmail.com>
2024-09-01 09:24:28 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableCaching
+@EnableScheduling
+public class CacheConfig {
+
+}
